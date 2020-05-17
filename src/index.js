42fc9e3f972e55c6bcc25466762526da30578087
alphadzingina <alphaadx6@gmail.com>
2020-05-17 18:39:13 +0100

    pass avatar props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return(
         <div className="ui container comments">
-            <CommentDetail author="Dayo" />
-            <CommentDetail author="Seun"/>
-            <CommentDetail author="Fikayo"/>
-            <CommentDetail author="Shalom"/>
+            <CommentDetail author="Dayo" timeAgo="Yesterday at 3:30pm" post="The boy cried wolf" avatar={faker.image.avatar()} />
+            <CommentDetail author="Seun" timeAgo="Yesterday at 6:25pm" post="The dog has three legs" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Fikayo" timeAgo="Today at 8:42am" post="A snake is a reptile" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Shalom" timeAgo="Today at 11:55am" post="black is a color" avatar={faker.image.avatar()}/>
         </div>    
     );
 };
